ff5479f53f6ee3369aabd124db0e0a26b9d6eeda
NoruMashroom <kimminku98@naver.com>
2021-10-26 23:23:28 +0900

    마이페이지 다크모드 + 앱정보 토스트 메세지

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.test1
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.view.ViewCompat.setBackground
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.frag_home.*
+import kotlinx.android.synthetic.main.frag_like.*
+import kotlinx.android.synthetic.main.frag_mypage.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,9 +44,8 @@ class MainActivity : AppCompatActivity() {
         Data(R.drawable.ic_launcher_background, name = "11번"),
         Data(R.drawable.ic_launcher_background, name = "12번")
     )
-
     var viewList = ArrayList<View> ()
-
+    var check = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -73,15 +82,62 @@ class MainActivity : AppCompatActivity() {
 
             when(it.itemId) {
                 R.id.home -> {viewpager.setCurrentItem(0)
-                    recycle.visibility = View.VISIBLE}
+                    recycle.visibility = View.VISIBLE
+                    if(check == 1){
+                        frag_home.setBackgroundColor(Color.BLACK)
+                    }
+                    else{
+                        frag_home.setBackgroundColor(Color.WHITE)
+                    }
+                }
                 R.id.like -> {viewpager.setCurrentItem(1)
-                    recycle.visibility = View.INVISIBLE}
+                    recycle.visibility = View.INVISIBLE
+                    if(check == 1){
+                        frag_like.setBackgroundColor(Color.BLACK)
+                    }
+                    else{
+                        frag_like.setBackgroundColor(Color.WHITE)
+                    }
+                }
                 R.id.profile -> {viewpager.setCurrentItem(2)
-                    recycle.visibility = View.INVISIBLE}
+                    recycle.visibility = View.INVISIBLE
+                    switch1.setOnClickListener {
+                        if (switch1.isChecked){
+                            check = 1
+                            switch1.setTextColor(Color.WHITE)
+                            textView_NAME.setTextColor(Color.WHITE)
+                            textView_name.setTextColor(Color.WHITE)
+                            textView_CAR.setTextColor(Color.WHITE)
+                            textView_car.setTextColor(Color.WHITE)
+                            textView_AGE.setTextColor(Color.WHITE)
+                            textView_age.setTextColor(Color.WHITE)
+                            textView2.setTextColor(Color.WHITE)
+                            frag_mypage.setBackgroundColor(Color.BLACK)
+                            bottomNavigationView.setBackgroundColor(Color.BLACK)
+                    }
+                        else{
+                            check = 0
+                            switch1.setTextColor(Color.BLACK)
+                            textView_NAME.setTextColor(Color.BLACK)
+                            textView_name.setTextColor(Color.BLACK)
+                            textView_CAR.setTextColor(Color.BLACK)
+                            textView_car.setTextColor(Color.BLACK)
+                            textView_AGE.setTextColor(Color.BLACK)
+                            textView_age.setTextColor(Color.BLACK)
+                            textView2.setTextColor(Color.BLACK)
+                            frag_mypage.setBackgroundColor(Color.WHITE)
+                            bottomNavigationView.setBackgroundColor(Color.WHITE)
+                        }
+                    }
+                    textView2.setOnClickListener {
+                        var versionName = BuildConfig.VERSION_NAME
+                        Toast.makeText(this, versionName, Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
             true
-
         }
+
 
         recycle.layoutManager = LinearLayoutManager(this)
         recycle.adapter = CustomAdapter(DataList, this)
