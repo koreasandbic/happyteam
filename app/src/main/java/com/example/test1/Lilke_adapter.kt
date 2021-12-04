cb4e3d4700ec2bc79ce0189b418398579731a8eb
Seunggue Park <qkr1234go@naver.com>
2021-12-04 16:22:49 +0900

    즐찾 클릭이벤트

--- app/src/main/java/com/example/test1/Lilke_adapter.kt
+++ app/src/main/java/com/example/test1/Lilke_adapter.kt
@@ -1,6 +1,7 @@
 package com.example.test1
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.view.LayoutInflater
@@ -39,6 +40,67 @@ class Like_adapter(val context: Context, var DataList:ArrayList<String>) : BaseA
         islandRef.getBytes(ONE_MEGABYTE).addOnSuccessListener {
             var bitmap : Bitmap = BitmapFactory.decodeByteArray(it, 0, it.size)
             profile_like.setImageBitmap(bitmap)
+        }
+
+        val curData = DataList[p0]
+        name_like.setOnClickListener {
+            if(curData=="화성방조제길")
+            {val intent = Intent( context, Hwasung::class.java)
+                context.startActivity(intent)}
+            else if(curData=="자유로") {
+                run {
+                    val intent = Intent(context, Paju::class.java)
+                    context.startActivity(intent)
+                }
+            }
+            else if(curData=="국립수목원로") {
+                run {
+                    val intent = Intent(context, Pocheon::class.java)
+                    context.startActivity(intent)
+                }
+            }
+            else if(curData=="청평호반길,북한강변길") {
+                run {
+                    val intent = Intent(context, Gapeong::class.java)
+                    context.startActivity(intent)
+                }
+            }
+            else if(curData=="남한산성,팔당호 벚꽃길") {
+                run {
+                    val intent = Intent(context, Gwangju::class.java)
+                    context.startActivity(intent)
+                }
+            }
+            else if(curData=="화음길") {
+                run {
+                    val intent = Intent(context, Namyang::class.java)
+                    context.startActivity(intent)
+                }
+            }
+            else if(curData=="두물머리 강변길") {
+                run {
+                    val intent = Intent(context, Yangpyeong::class.java)
+                    context.startActivity(intent)
+                }
+            }
+            else if(curData=="가실벚꽃길") {
+                run {
+                    val intent = Intent(context, Yongin::class.java)
+                    context.startActivity(intent)
+                }
+            }
+            else if(curData=="금광호수로") {
+                run {
+                    val intent = Intent(context, Ansung::class.java)
+                    context.startActivity(intent)
+                }
+            }
+            else if(curData=="시화방조제길") {
+                run {
+                    val intent = Intent(context, Ansan::class.java)
+                    context.startActivity(intent)
+                }
+            }
         }
         delete.setOnClickListener {
             val updates = hashMapOf<String, Any>(
